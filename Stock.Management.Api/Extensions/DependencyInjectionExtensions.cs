25c0715e9985166e2b318f291af4a82a4611c7e2
Mariaedoarda <maria.moreira115@etec.sp.gov.br>
2024-06-12 00:19:47 -0300

    commit inicial

---
+++ Stock.Management.Api/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Stock.Management.Api.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        internal static IServiceCollection ResolveDependecies(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
